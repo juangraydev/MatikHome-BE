3328ac4f211c9fae2e60a5adac7f47480eb01dbc
juangraydev <jwsabala@gmail.com>
2023-05-22 17:00:35 +0800

    be

--- common/settings.py
+++ common/settings.py
@@ -151,7 +151,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
-STATICFILES_DIRS=[
-    BASE_DIR.joinpath('static')
-]
