02ac90a4626f37a078eaddd380e438c208f13732
juangraydev <jwsabala@gmail.com>
2023-11-20 09:32:08 +0800

    latest

--- homes/views.py
+++ homes/views.py
@@ -175,10 +175,10 @@ class HomeNotification(APIView):
         token_auth = TokenAuthentication()
         token_key = token_auth.extract_bearer(request.META[idf.HTTP_AUTHORIZATION])
         user_instance =token_auth.decode_token(token_key)
-        homes_list = user_homes_management.update_user_house_invite(userId=user_instance[idf.OBJ_ID], data=request.data, status=invite_status)
+        user_homes_management.update_user_house_invite(userId=user_instance[idf.OBJ_ID], data=request.data, status=invite_status)
       
         resp_details = create_response_details()
         resp_payload = create_response(
-                                       resp_data=homes_list,
+                                       resp_data={},
                                        resp_details=resp_details)
         return Response(resp_payload, status=status.HTTP_200_OK)
